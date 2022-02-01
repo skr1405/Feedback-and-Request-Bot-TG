bb7bccd9f4b32ca1d2f8fceb804a1f186fed1fdc
skr1405 <64830000+skr1405@users.noreply.github.com>
2022-02-01 15:12:30 +0530

    update

--- Bot/__main__.py
+++ Bot/__main__.py
@@ -4,27 +4,32 @@ import logging
 from pyrogram import Client
 from os import environ
 
-# Logging Stuff
-logging.basicConfig(
-        level=logging.INFO,
-        format="%(asctime)s %(levelname)s %(threadName)s %(name)s %(message)s",
+
+
+if __name__ == "__main__":
+    # Logging Stuff
+    logging.basicConfig(
+            level=logging.INFO,
+            format="%(asctime)s %(levelname)s %(threadName)s %(name)s %(message)s",
+        )
+    logging.getLogger("pyrogram").setLevel(logging.ERROR)
+
+
+    BOT_TOKEN = environ.get("BOT_TOKEN")
+    API_ID = environ.get("API_ID")
+    API_HASH = environ.get("API_HASH")
+
+    app = Client(
+        "Feedback and Request Bot",
+        API_ID,
+        API_HASH,
+        bot_token=BOT_TOKEN
     )
-logging.getLogger("pyrogram").setLevel(logging.ERROR)
-
-BOT_TOKEN = environ.get("BOT_TOKEN")
-API_ID = environ.get("API_ID")
-API_HASH = environ.get("API_HASH")
-
-app = Client(
-    "Feedback and Request Bot",
-    API_ID,
-    API_HASH,
-    bot_token=BOT_TOKEN
-)
-app.start()
+    
+    app.start()
 
 
-app.send_message(environ.get("OWNER_ID"), "heLLO")
+    app.send_message(environ.get("OWNER_ID"), "heLLO")
 
 
 
