fdfa0c6bb7cc9f460fa5dd93c7ca51ee9f38872a
skr1405 <64830000+skr1405@users.noreply.github.com>
2022-02-01 13:08:59 +0530

    update

--- bot.py
+++ bot.py
@@ -1,1 +1,17 @@
-vbvb
+from pyrogram import Client, filters
+from os import environ
+
+
+BOT_TOKEN = environ.get("BOT_TOKEN")
+
+app = Client(bot_token=BOT_TOKEN)
+
+
+app.send_message(environ.get("OWNER_ID"), "heLLO")
+
+
+
+
+
+
+
