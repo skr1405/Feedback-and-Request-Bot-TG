d9963eb4994db24d84808ff3f9d69d810854ad0a
skr1405 <64830000+skr1405@users.noreply.github.com>
2022-02-01 16:41:29 +0530

    u

--- Bot/Handlers/feedback.py
+++ Bot/Handlers/feedback.py
@@ -4,7 +4,7 @@ from pyrogram import Client, filters
 
 logging.getLogger(__name__)
 
-@Client.on_message(filters.command("start") & filters.private)
+@Client.on_message(filters.command("ye") & filters.private)
 async def start(client, message):
     await client.send_message(message.chat.id, "Hello")
     
