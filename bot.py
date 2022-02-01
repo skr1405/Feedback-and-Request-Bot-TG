22a9bb95dfd184897f093bedfca0ea2584f2aca0
skr1405 <64830000+skr1405@users.noreply.github.com>
2022-02-01 13:12:19 +0530

    update

--- bot.py
+++ bot.py
@@ -1,10 +1,17 @@
+# Importing Required Modules
+import logging
 from pyrogram import Client, filters
 from os import environ
 
+logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s %(levelname)s %(threadName)s %(name)s %(message)s",
+    )
+logging.getLogger("pyrogram").setLevel(logging.ERROR)
 
 BOT_TOKEN = environ.get("BOT_TOKEN")
 
-app = Client(bot_token=BOT_TOKEN)
+app = Client("Feedback and Request Bot", bot_token=BOT_TOKEN)
 
 
 app.send_message(environ.get("OWNER_ID"), "heLLO")
