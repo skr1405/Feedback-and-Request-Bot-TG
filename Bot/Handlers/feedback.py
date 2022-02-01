425dabbe3d98bd5acae9b4d9a9e3340136f9c9f2
skr1405 <64830000+skr1405@users.noreply.github.com>
2022-02-01 16:13:47 +0530

    u

--- Bot/Handlers/feedback.py
+++ Bot/Handlers/feedback.py
@@ -1,6 +1,10 @@
 import logging
 from pyrogram import Client, filters
 
+logging.basicConfig(
+            level=logging.INFO,
+            format="%(asctime)s %(levelname)s %(threadName)s %(name)s %(message)s",
+        )
 logging.getLogger(__name__)
 
 @Client.on_message(filters.command("start") & filters.private)
