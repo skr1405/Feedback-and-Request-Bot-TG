c7886432532049bd86725bba6f0f8ec576dc5fa7
skr1405 <64830000+skr1405@users.noreply.github.com>
2022-02-01 16:41:22 +0530

    u

--- Bot/__main__.py
+++ Bot/__main__.py
@@ -24,7 +24,7 @@ if __name__ == "__main__":
         API_ID,
         API_HASH,
         bot_token=BOT_TOKEN,
-        plugins=dict(root="Handlers")
+        plugins=dict(root="./Handlers")
     )
 
     @app.on_message(filters.command("start") & filters.private)
