e622998e790a980460147c746e8e72d7c3a3b909
skr1405 <64830000+skr1405@users.noreply.github.com>
2022-02-02 13:42:14 +0530

    u

--- bot/handlers/feedback.py
+++ bot/handlers/feedback.py
@@ -64,4 +64,5 @@ def pm_text(update, context):
         text = IF_TEXT.format(reference_id, reference_id, info.first_name, "" if info.last_name == None else " "+info.last_name, update.message.text),
         parse_mode = "html"
     )
+    logging.info(update.message.entities)
 
