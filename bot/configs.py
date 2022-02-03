45066767dadd5a5dc8e3ce50acc63b17a5482553
skr1405 <64830000+skr1405@users.noreply.github.com>
2022-02-03 17:02:33 +0530

    update

--- bot/configs.py
+++ bot/configs.py
@@ -6,6 +6,6 @@ class Config:
     BOT_TOKEN = environ.get("BOT_TOKEN")
     BASE_URL_OF_BOT = environ.get("BASE_URL_OF_BOT")
     PORT = int(environ.get("PORT", "8443"))
-    OWNER_ID = environ.get("OWNER_ID")
+    OWNER_ID = int(environ.get("OWNER_ID"))
     GROUP_LINK = environ.get("GROUP_LINK")
     CHANNEL_LINK = environ.get("CHANNEL_LINK")
